a2534c198d1c728204c44a1ac54cf4b3b76a8ca3
Yt <13579@buaa.edu.cn>
2024-04-02 21:56:59 +0800

    t2过test

---
+++ T2/glue.js
@@ -0,0 +1,24 @@
+import assert from "assert";
+import Module from "./t2.ems.js";
+
+
+let a = await Module()
+const mancala_result_ptr_cwrap = a.cwrap('mancala_result_ptr', 'number', ['number', 'number', 'number']);
+
+function get_int32_pointer(inputArray) {
+    let inputArrayPointer = a._malloc(inputArray.length * Int32Array.BYTES_PER_ELEMENT);
+    a.HEAP32.set(inputArray, inputArrayPointer / Int32Array.BYTES_PER_ELEMENT);
+    return inputArrayPointer;
+}
+
+
+export function mancalaResult(flag,array,size) {
+    let flagPointer = get_int32_pointer([flag]);
+    let arrayPointer = get_int32_pointer(array);
+    let sizePointer = get_int32_pointer([size]);
+    let ans = mancala_result_ptr_cwrap(flagPointer,arrayPointer,sizePointer);
+    a._free(flagPointer);
+    a._free(arrayPointer);
+    a._free(sizePointer);
+    return ans;
+}
--- T2/test.js
+++ T2/test.js
@@ -4,6 +4,7 @@ import assert from "assert";
 // import { mancalaResult } from "./t2-as/build/release.js";
 // import { mancala_result as mancalaResult } from "./t2_rust/pkg/t1_rust.js"
 // [Write your own "import" for other PLs.]
+import {mancalaResult} from "./glue.js"
 
 assert.strictEqual(mancalaResult(1,[11,12],2),30001);
 assert.strictEqual(mancalaResult(1,[14],1),20001);
